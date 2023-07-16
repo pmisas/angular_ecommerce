3637860c7add630d4224e1a8c798f6f02605ad06
Paula Misas <pmisas@unal.edu.co>
2023-07-16 11:48:54 -0500

    arreglo fondo, enrutamiento, navbar

--- angular-ecommerce/src/app/modules/ecommerce/ecommerce-routing.module.ts
+++ angular-ecommerce/src/app/modules/ecommerce/ecommerce-routing.module.ts
@@ -5,7 +5,7 @@ import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
     {path:'', component:HomeComponent},
-    {path:'category', component:CategoryComponent}
+    {path:':category', component:CategoryComponent}
 ];
 
 @NgModule({
