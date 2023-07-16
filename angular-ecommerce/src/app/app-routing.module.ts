184d4dbbb2cea1094c84c67d2b69d8ec42c8225a
Paula Misas <pmisas@unal.edu.co>
2023-07-16 04:59:02 -0500

    arreglo antes de irme a dormi. enrutamiento

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
   path:'',
   component:SkeletonComponent,
   loadChildren: ()=> 
     import ('@modules/ecommerce/ecommerce.module').then((m)=>m.EcommerceModule)
-}];
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
--- angular-ecommerce/src/app/shared/components/index.ts
+++ angular-ecommerce/src/app/shared/components/index.ts
@@ -1,10 +1,11 @@
 //Import Components
 
-import { CartComponent } from "./cart/cart.component";
+//import { CartComponent } from "./cart/cart.component";
 
 export const components: any[] =[
-    CartComponent
+    //CartComponent
 ];
 
+
 //export all components
-export * from './cart/cart.component'
+//export * from './cart/cart.component'
